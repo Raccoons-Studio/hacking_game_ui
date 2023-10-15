8a87e337b8eaca270dc69ea2038f7dada867ccae
Cédric Millauriaux <cmillauriaux@dinno.fr>
2023-10-15 15:18:28 +0200

    feat: Amélioration de l'application SingleFans

--- lib/virtual_machine/applications/single_fans/single_fans.dart
+++ lib/virtual_machine/applications/single_fans/single_fans.dart
@@ -17,6 +17,7 @@ class SingleFans extends StatefulWidget {
 
 class _SingleFansState extends State<SingleFans> {
   Future? _datas;
+  int _selectedTab = 0;
 
   @override
   void initState() {
@@ -30,63 +31,192 @@ class _SingleFansState extends State<SingleFans> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: CupertinoColors.extraLightBackgroundGray,
-      child: FutureBuilder(
-          future: _datas,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasData) {
-              List<ScrollableData> dataList = snapshot.data[1]!;
-              return ListView.builder(
-                itemCount: dataList.length,
-                reverse: true,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: Column(
+        color: Colors.black,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Header
+              Container(
+                  color: Colors.black,
+                  height: 50,
+                  child: Center(
+                      child: Text('Single Fans',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 20)))),
+              // Main Area
+              Expanded(
+                  child: FutureBuilder(
+                      future: _datas,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        } else if (snapshot.hasData) {
+                          List<ScrollableData> dataList = snapshot.data[1]!;
+
+                          if (_selectedTab == 0) {
+                            // Feed
+                            return buildFeed(dataList);
+                          } else if (_selectedTab == 1) {
+                            // Models
+                            return buildModels(dataList);
+                          } else if (_selectedTab == 2) {
+                            // Me
+                            return buildMe();
+                          } else {
+                            return const SizedBox.shrink();
+                          }
+                        }
+
+                        return const SizedBox.shrink();
+                      })),
+              // Footer
+              Container(
+                  color: Colors.black,
+                  height: 50,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: AssetImage(dataList[index].avatar),
-                          ),
-                          title: Text(dataList[index].content),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Image.asset(
-                              "assets/images/${dataList[index].asset}"),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            dataList[index].subcontent,
-                            style: const TextStyle(fontSize: 18.0),
-                          ),
-                        ),
-                        ButtonBar(
-                          children: [
-                            TextButton(
-                              child: const Text('Like'),
-                              onPressed: () {/* ... */},
-                            ),
-                            TextButton(
-                              child: const Text('Share'),
-                              onPressed: () {/* ... */},
-                            ),
-                            TextButton(
-                              child: const Text('Contact'),
-                              onPressed: () {/* ... */},
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            }
-            return const SizedBox.shrink();
-          }),
+                        IconButton(
+                            icon: Icon(Icons.rss_feed, color: Colors.white),
+                            onPressed: () {
+                              setState(() {
+                                _selectedTab = 0;
+                              });
+                            }),
+                        IconButton(
+                            icon: Icon(Icons.people, color: Colors.white),
+                            onPressed: () {
+                              setState(() {
+                                _selectedTab = 1;
+                              });
+                            }),
+                        IconButton(
+                            icon: Icon(Icons.person, color: Colors.white),
+                            onPressed: () {
+                              setState(() {
+                                _selectedTab = 2;
+                              });
+                            }),
+                      ]))
+            ]));
+  }
+
+  Widget buildFeed(List<ScrollableData> dataList) {
+    return ListView.builder(
+      itemCount: dataList.length,
+      reverse: true,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Colors.black,
+          child: Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(dataList[index].avatar),
+                ),
+                title: Text(
+                  'Week ${dataList[index].week} Day ${dataList[index].day} Hour ${dataList[index].hour}',
+                  style: TextStyle(color: Colors.white),
+                ),
+                subtitle: Text(
+                  dataList[index].content,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset("assets/images/${dataList[index].asset}"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  dataList[index].subcontent,
+                  style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: [
+                  TextButton.icon(
+                    label: Text('', style: TextStyle(color: Colors.white)),
+                    icon: Icon(Icons.thumb_up, color: Colors.white),
+                    onPressed: () {/* ... */},
+                  ),
+                  TextButton.icon(
+                    label: Text('', style: TextStyle(color: Colors.white)),
+                    icon: Icon(Icons.message, color: Colors.white),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildModels(List<ScrollableData> dataList) {
+    var uniqueContentDataList = [];
+    for (var data in dataList) {
+      bool isExist = false;
+      for (var uniqueData in uniqueContentDataList) {
+        if (data.content == uniqueData.content) {
+          isExist = true;
+          break;
+        }
+      }
+      if (!isExist) {
+        uniqueContentDataList.add(data);
+      }
+    }
+    return ListView.builder(
+      itemCount: uniqueContentDataList.length,
+      itemBuilder: (context, index) {
+        String content = dataList[index].content;
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(dataList[index].avatar),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Text(content, style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            TextButton.icon(
+              label: Text('', style: TextStyle(color: Colors.white)),
+              icon: Icon(Icons.message, color: Colors.white),
+              onPressed: () {/* ... */},
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildMe() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Me', style: TextStyle(color: Colors.white, fontSize: 24)),
+          SizedBox(height: 20),
+          CircleAvatar(
+            backgroundImage: AssetImage(
+                'assets/images/avatar.png'), // Replace this with actual avatar
+            radius: 50,
+          ),
+          SizedBox(height: 20),
+          Text('1 subscription',
+              style: TextStyle(color: Colors.white, fontSize: 18)),
+          SizedBox(height: 20),
+          Text('2000 \$', style: TextStyle(color: Colors.white, fontSize: 18)),
+        ],
+      ),
     );
   }
 }
